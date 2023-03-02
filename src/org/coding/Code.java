ea3fbd8eb16f76bd237e9a18af869abb9a3bc50d
MOHANRAJK06 <mohanraj6596@gmail.com>
2023-03-02 15:02:59 +0530

    code added to B

--- src/org/coding/Code.java
+++ src/org/coding/Code.java
@@ -4,5 +4,6 @@ public class Code {
 	public static void main(String[] args) {
 		System.out.println("Java project");
 		System.out.println("Pushing code to master");
+		System.out.println("Branch B specific Code");
 	}
 }
